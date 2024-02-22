e47cf64bd82966489455f15a2eb5128c5a7e3533
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-22 13:04:38 +0100

    bookmarks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[ new create destroy]
+  end
 end
