93b61459bb3df5f64bdc1c916367560c63109e29
Oscar Fonty <oscar77.fonty@gmail.com>
2024-02-23 11:45:29 +0100

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get "up" => "rails/health#show", as: :rails_health_check
-  root to: "lists#index"
+  root to: "pages#home"
 
-  resources :lists, only: %i[show new create] do
+  resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
